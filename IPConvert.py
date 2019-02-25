bbe58bdfa0d43cf680f20ffde73a4e33ad311ecd
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2019-02-25 17:07:21 +0800

    Initial commit

---
+++ IPConvert.py
@@ -0,0 +1,35 @@
+import ipaddress
+
+
+def addr_transfer(input_addr):
+    net_addr = ipaddress.IPv4Network(input_addr.strip('\n'))
+    return str(net_addr.network_address) + '-' + str(net_addr.broadcast_address) + '; '
+
+
+write_buffer = []
+PKU_net = ['162.105.0.0/16', '202.112.7.0/24', '202.112.8.0/24', '222.29.0.0/17', '222.29.128.0/19', '115.27.0.0/16']
+for _ in PKU_net:
+    write_buffer.append(addr_transfer(_))
+
+IPv6_addr = ipaddress.IPv6Network('2001:da8:201::/48')
+write_buffer.append(str(IPv6_addr.network_address) + '-' + str(IPv6_addr.broadcast_address) + '; ')
+
+i = len(PKU_net) + 1
+
+with open('china_ip_list.txt', 'r') as cidr_list:
+    for line in cidr_list.readlines():
+        if i | 0b1111110000000000 == 0b1111110000000000:
+            if i != 0:
+                write_buffer[-1] = write_buffer[-1].strip('; ')
+                file_name = 'ip_list_' + str(i // 1024) + '.txt'
+                f = open(file_name, 'w')
+                f.writelines(write_buffer)
+                f.close()
+                write_buffer.clear()
+        write_buffer.append(addr_transfer(line))
+        i = i + 1
+
+i = i // 1024 + 1
+write_buffer[-1] = write_buffer[-1].strip('; ')
+with open('ip_list_' + str(i) + '.txt', 'w') as f:
+    f.writelines(write_buffer)
